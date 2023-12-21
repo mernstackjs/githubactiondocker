7d8bff63d6619ad6dea703803d49ffec9d8c0024
mernstackjs <mernstack.js@gmail.com>
2023-12-21 22:38:48 +0100

    add test router

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => res.send("API is running seccessfully"));
-
+app.get("/test", (req, res) => res.json({ user: "ahmed" }));
 const port = 9900;
 app.listen(port, () => console.log(`server is running on port ${port}`));
